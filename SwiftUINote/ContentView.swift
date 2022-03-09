4a715406d78ba15e8c12306165cc92dd385e1636
calmkeen <iyunseong159@gmail.com>
2022-03-09 17:27:28 +0900

    Initial Commit

---
+++ SwiftUINote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINote
+//
+//  Created by calmkeen on 2022/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINote/SwiftUINoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINoteApp.swift
+//  SwiftUINote
+//
+//  Created by calmkeen on 2022/03/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
